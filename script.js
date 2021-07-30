c0a3e3adfb87478c91819c08722606dbc320e86e
Esther-Moki <esther.moki@student.moringaschool.com>
2021-07-30 15:33:10 +0300

    added toggle to the order part

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //what we do toggle function//
 $(document).ready(function(){
-  $("#picone").click(function(){
-    $("#potato").toggle();
-    $("#picone").hide();
-  $("#potato").click(function(){
-    $("#picone").show();
-    $("#potato").hide();
+  $("#pic1").click(function(){
+    $(".register").toggle();
+    $("#pic1").hide();
+  $(".register").click(function(){
+    $("#pic1").show();
+    $(".register").hide();
     });
   });
 });

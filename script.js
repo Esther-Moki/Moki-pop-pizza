e92ee97a09754ac8611ae98cc3bfb77f4481b7c1
Esther-Moki <esther.moki@student.moringaschool.com>
2021-07-30 19:35:28 +0300

    did an about part

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//what we do toggle function//
+//how to make an order toggle function//
 $(document).ready(function(){
   $("#pic1").click(function(){
     $(".register").toggle();
@@ -18,7 +18,7 @@ $(document).ready(function(){
     $(".pick").hide();
     });
   });
-})
+});
 $(document).ready(function(){
   $("#pic3").click(function(){
     $(".order").toggle();
@@ -28,7 +28,7 @@ $(document).ready(function(){
     $(".order").hide();
     });
   });
-})
+});
 $(document).ready(function(){
   $("#pic4").click(function(){
     $(".doorstep").toggle();
@@ -38,4 +38,41 @@ $(document).ready(function(){
     $(".doorstep").hide();
     });
   });
-})
+});
+
+//end of the function
+
+function validateInputs(){
+  var nameInput = document.getElementById("username");
+  var passwordInput = document.getElementById("password");
+
+  if(nameInput.value == "" ){
+    alert("Please enter your name");
+    return false;
+  }
+  if(passwordInput.value == ""){
+    alert("Please create a password");
+    return false;
+  }  
+     
+  let price = 0;
+   
+  function Pizza(size,crust,type,toppings,price){
+      this.size = size,  
+      this.crust = crust,
+      this.type = type,
+      this.toppings = toppings,
+      this.price = price
+  }
+
+  Pizza.prototype.price = function(){
+    if (this.size === "small"){
+      this.price += 300;
+    }else if(this.size === "medium"){
+      this.price += 500;
+    }else if(this.size === "large"){
+      this.price += 700;
+    }
+  
+  }
+}

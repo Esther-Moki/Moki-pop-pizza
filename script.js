c33e3119f53cf05f7541554b474209474eb93be1
Esther-Moki <esther.moki@student.moringaschool.com>
2021-07-31 22:20:35 +0300

    added padding to the about class

--- script.js
+++ script.js
@@ -57,14 +57,13 @@ $(document).ready(function(){
  // end of sizes part
  
  var pizzaChoices = document.querySelector("#pizzaChoices");
- 
- let price = 0
- function Pizza(size,crust,type,toppings,price){
+
+
+ function Pizza(size,crust,type,toppings){
        this.size = size,  
        this.crust = crust,
        this.type = type,
-       this.toppings = toppings,
-       this.price = 0
+       this.toppings = toppings
   }
   Pizza.prototype.price = function(){
       if (this.size === "small"){
@@ -73,9 +72,7 @@ $(document).ready(function(){
         this.price += 600;
        }else (this.size === "large")
         this.price += 800;
-       
-
-        return this.price;
+        return this.price();
       }
 
 
@@ -84,6 +81,7 @@ $(document).ready(function(){
  function onSubmit(e){
    e.preventDefault();
 
+   
    if(nameInput.value === "" ){
      alert("Please enter your fullname");
      return false;
@@ -96,14 +94,50 @@ $(document).ready(function(){
 
 
   
-   //  var smallSize=document.getElementById("small").checked;
-     //var mediumSize=document.getElementById("medium").checked;
-    // var largeSize=document.getElementById("large").checked;
+    var smallSize=document.getElementById("small").checked;
+     var mediumSize=document.getElementById("medium").checked;
+     var largeSize=document.getElementById("large").checked;
 
-     // if(smallSize == false && mediumSize == false && largeSize == false){
-       // alert("Please select your pizza size");
-        //return false;
-     // } 
+     if(smallSize == false && mediumSize == false && largeSize == false){
+        alert("Please select your pizza size");
+        return false;
+     } 
+     var crispyCrust=document.getElementById("crispy").checked;
+     var cc=document.getElementById("crammed").checked;
+     var dd=document.getElementById("double").checked;
+     var gluten=document.getElementById("glutenFree").checked;
+
+    if( crispyCrust == false && cc == false && dd == false && gluten == false){
+     alert("Please pick the crust you would like");
+     return false;
+   }
+     var meaty=document.getElementById("meaty").checked;
+     var veggy=document.getElementById("veggy").checked;
+     if (meaty ==false && veggy == false){
+         alert("Please select if you would love meat or veggies");
+        return false;
+
+     }
+
+        var mushroom=document.getElementById("mushroom").checked;
+        var olives=document.getElementById("olives").checked;
+        var pineapple=document.getElementById("pineapple").checked;
+        var onions=document.getElementById("onions").checked;
+        var tomato=document.getElementById("tomato").checked;
+        var mozarella=document.getElementById("mozarella").checked;
+        var macon=document.getElementById("macon").checked;
+         var salami=document.getElementById("salami").checked;
+        var chicken=document.getElementById("chicken").checked;
+        var groundbeef=document.getElementById("groundbeef").checked;
+   
+      if (mushroom ==false && olives == false && pineapple == false && onions == false && tomato == false && 
+        mozarella ==false && macon == false && salami == false && chicken == false && groundbeef == false ){
+         alert("Please select the toppings you would like");
+          return false;
+       }
+    
+     
+
      var sizesInput = $("input[type=radio][name=size]:checked").val();
      var crustInput = $("input[type=radio][name=crust]:checked").val();
      var typeInput = $("input[type=radio][name=type]:checked").val();
@@ -114,13 +148,14 @@ $(document).ready(function(){
      toppingsValue += " ,"+(toppings[i]).value;
        i++;
    }  
-    
+      
+
   //  var newPizzaOrder = new Pizza(size,crust,type,toppings,price);
   //  newPizzaOrder.price();
     
-   var li = document.createElement("li");
-  li.appendChild(document.createTextNode(`${nameInput.value}:  ${sizesInput} : ${crustInput}:  ${typeInput}
-  ${toppingsValue} :${this.price}`));
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${nameInput.value}:  ${sizesInput} : ${crustInput}:  ${typeInput}
+    ${toppingsValue}`));
   
         pizzaChoices.appendChild(li);
 
